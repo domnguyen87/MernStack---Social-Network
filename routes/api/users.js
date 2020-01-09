ab1e7e81c464243359f8c80ebe96a08f407f9cc8
Dominique Nguyen <domnguyen@gmail.com>
2020-01-09 15:09:59 -0800

    Implement JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/Users');
@@ -14,7 +16,7 @@ router.post(
     [
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Please include a valid email').isEmail(),
-        check('password', 'Please enter a pw with 6+ characters').isLength({ min:6 })
+        check('password', 'Please enter a pw with 6+ characters').isLength({ min: 6 })
     ],
     async (req, res) => {
         const errors = validationResult(req);
@@ -51,18 +53,32 @@ router.post(
 
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
-            await user.save(); 
+            await user.save();
 
             //Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
 
-            res.send('User registered')
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            )
+
 
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server Error');
         }
 
-        
+
     });
 
 module.exports = router;
