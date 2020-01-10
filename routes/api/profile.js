1bbbc448d9809289d40141d74de073e80cda63fc
Dominique Nguyen <domnguyen@gmail.com>
2020-01-10 15:55:48 -0800

    Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,6 +146,75 @@ router.get('/user/:user_id', async (req, res) => {
     }
 })
 
+//@route    DELETE api/profile
+//@desc     Delete profile, user and post
+//@access   private
+router.delete('/', auth, async (req, res) => {
+    try {
+
+        //Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id })
+
+        //Remove User
+        await User.findOneAndRemove({ _id: req.user.id })
+
+        res.json(msg: "User deleted");
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+
+    }
+})
+
+//@route    PUT api/profile/experience
+//@desc     Add profile experience
+//@access   private
+router.put('/experience', [auth, [
+    check('title', 'Title is req').not().isEmpty(),
+    check('comapny', 'Company is req').not().isEmpty(),
+    check('from', 'From date is req').not().isEmpty(),
+]], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id })
+            
+            profile.experience.unshift(newExp);
+
+            await profile.save();
+            
+            res.json(prfile); 
+
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+            
+        }
+
+})
 
 
 
