3cfbc1ddef07a2f0dd0bc14670f20a0b7887ea84
Dominique Nguyen <domnguyen@gmail.com>
2020-01-10 16:21:33 -0800

    Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -216,6 +216,29 @@ router.put('/experience', [auth, [
 
 })
 
+//@route    DELETE api/profile/experience/:exp_id
+//@desc     Delete profile experience
+//@access   private
+router.delete('/experience/:exp_id', auth, (req, res));
+try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    //Get remove Index
+    const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+    
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+
+} catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+}
+
 
 
 module.exports = router;
