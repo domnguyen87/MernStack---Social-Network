5879ed21b6eaa2f8cbaacf3d72c3c8952a56440c
Dominique Nguyen <domnguyen@gmail.com>
2020-01-13 12:13:03 -0800

    getting started on dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,14 +7,21 @@ import { logout } from '../../actions/auth'
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
+            <li>
+                <Link to="/dashboard">
+                    <span className="hide-sm">Dashboard</span>
+                    <i className="fas fa-user"></i> {' '}
+                </Link>
+            </li>
+
             <li><a onClick={logout} href="#!">
                 <i className="fas fa-sign-out-alt"></i> {' '}
-                <span className="hide-sm">Logout</span> 
-                </a>
+                <span className="hide-sm">Logout</span>
+            </a>
             </li>
         </ul>
-    )  
-    
+    )
+
     const guestLinks = (
         <ul>
             <li><a href="#!">Developers</a></li>
@@ -24,13 +31,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     )
 
 
-     
+
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <Link to="index.html"><i className="fas fa-code"></i> DevConnector</Link> 
+                <Link to="index.html"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
-            {!loading && (<Fragment>{ isAuthenticated? authLinks: guestLinks}</Fragment>)}
+            {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
         </nav>
     )
 }
@@ -41,7 +48,7 @@ Navbar.propTypes = {
 }
 
 const mapStateToProps = state => ({
-    auth:state.auth
+    auth: state.auth
 })
 
-export default connect(mapStateToProps, { logout }) (Navbar)
+export default connect(mapStateToProps, { logout })(Navbar)
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,7 @@
 import {
     GET_PROFILE,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    CLEAR_PROFILE
     
 } from '../actions/types';        
 
@@ -28,6 +29,13 @@ export default function(state = initialState, action) {
                 ...state,
                 error: payload,
                 loading: false
+            };
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
+                loading:false
             };
         default:
         return state;
