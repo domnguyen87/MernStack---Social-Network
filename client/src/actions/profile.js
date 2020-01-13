b3df694a073141312e863e6c5d1c7a6d39986e17
Dominique Nguyen <domnguyen@gmail.com>
2020-01-13 13:09:48 -0800

    create profile action submit

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,6 +5,7 @@ import {
     GET_PROFILE,
     PROFILE_ERROR
 } from './types';
+import { setAlert } from './alert';
 
 //Get Current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -16,6 +17,42 @@ export const getCurrentProfile = () => async dispatch => {
             payload: res.data
         })
     } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+//create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+        if (!edit) {
+            history.push('/dashboard')
+        }
+
+    } catch (err) {
+
+        const errors = err.response.data.errors;
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
         dispatch({
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
