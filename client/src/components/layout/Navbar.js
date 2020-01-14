dac059f2628fe946a7d4fadb45b91ea8c3188320
Dominique Nguyen <domnguyen@gmail.com>
2020-01-14 14:31:15 -0800

    change homepage

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <Link to="index.html"><i className="fas fa-code"></i> DevConnector</Link>
+                <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
             {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
         </nav>
