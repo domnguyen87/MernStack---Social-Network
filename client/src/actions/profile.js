1e5592225625568371102fc644e33ea008c05863
Dominique Nguyen <domnguyen@gmail.com>
2020-01-14 10:35:55 -0800

    starting on profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
-    CLEAR_PROFILE,
-    GET_PROFILES 
+    CLEAR_PROFILE
+    
 } from './types';
 import { setAlert } from './alert';
 
@@ -26,23 +27,24 @@ export const getCurrentProfile = () => async dispatch => {
     }
 }
 
-//Get all profile
+// Get all profiles
 export const getProfiles = () => async dispatch => {
     dispatch({ type: CLEAR_PROFILE });
+
     try {
         const res = await axios.get('/api/profile');
 
         dispatch({
             type: GET_PROFILES,
             payload: res.data
-        })
+        });
     } catch (err) {
         dispatch({
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
-        })
+        });
     }
-}
+};
 
 //Get profile by ID
 export const getProfileById = userId => async dispatch => {
--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const ProfileItem = ({
     profile: {
@@ -12,23 +12,31 @@ const ProfileItem = ({
     }
 }) => {
     return (
-        <div className="profile bg-light">
-            <img src={avatar} alt="" className="round-img" />
+        <div className='profile bg-light'>
+            <img src={avatar} alt='' className='round-img' />
             <div>
                 <h2>{name}</h2>
-                <p>{status} {company && <span>{company}</span>}</p>
-                <p className="my-1"> {location && <span>{location}</span>}</p>
+                <p>
+                    {status} {company && <span> at {company}</span>}
+                </p>
+                <p className='my-1'>{location && <span>{location}</span>}</p>
                 <Link to={`/profile/${_id}`} className='btn btn-primary'>
                     View Profile
-    </Link>
+        </Link>
             </div>
-
+            <ul>
+                {skills.slice(0, 4).map((skill, index) => (
+                    <li key={index} className='text-primary'>
+                        <i className='fas fa-check' /> {skill}
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
-}
+    );
+};
 
 ProfileItem.propTypes = {
     profile: PropTypes.object.isRequired
-}
+};
 
-export default ProfileItem
+export default ProfileItem;
